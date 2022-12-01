d20375289af660213071b49dd69eb99c825ea1b7
Onno Vos <onno-vos-dev@users.noreply.github.com>
2022-12-01 09:07:17 +0100

    Only keep top 3 rather than keeping all calories in the acc to handle ridiculously large inputs

--- apps/aoc/src/day01.erl
+++ apps/aoc/src/day01.erl
@@ -3,8 +3,7 @@
 -export([ solve/0 ]).
 
 solve() ->
-  SummedCalories = summed_calories(),
-  TopThree = lists:sublist(lists:reverse(lists:sort(SummedCalories)), 3),
+  TopThree = summed_calories(),
   Part1 = hd(TopThree),
   Part2 = lists:sum(TopThree),
   {Part1, Part2}.
@@ -14,9 +13,9 @@ summed_calories() ->
     aoc:read_file_fold("day01.txt",
                        <<"\n">>,
                        fun (<<>>, {Group, Acc}) ->
-                             {0, [Group | Acc]};
+                             {0, lists:sublist(lists:reverse(lists:sort([Group | Acc])), 3)};
                            (B, {Group, Acc}) ->
                              {binary_to_integer(B) + Group, Acc}
                        end,
-                       {0, []}),
+                       {0, [0, 0, 0]}),
   [LastGroup | Acc].
