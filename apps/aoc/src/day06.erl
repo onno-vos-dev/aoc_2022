cc22e7090a2022fac7a2fcc63abd87d3924fd48d
Onno Vos <onno-vos-dev@users.noreply.github.com>
2022-12-06 19:40:22 +0100

    Add optimized clause for day 6

--- apps/aoc/src/day06.erl
+++ apps/aoc/src/day06.erl
@@ -19,6 +19,15 @@ do_find_markers(<<_:8, Rest/binary>> = Bin, Size, {Part1, Cnt}) ->
       do_find_markers(Rest, Size, {Part1, Cnt + 1})
   end.
 
+%% Optimized clause that was the solution for part1 can remain
+%% even though it doesn't work for part2.
+%% This lookup is super cheap so this optimized way of checking
+%% whether or not we have a marker can simply remain.
+is_marker(<<A:8, B:8, C:8, D:8, _/binary>>, _, _)
+  when (A =:= B) orelse (A =:= C) orelse (A =:= D) orelse
+       (B =:= C) orelse (B =:= D) orelse
+       (C =:= D) ->
+  false;
 is_marker(<<A:8, _/binary>>, _, Acc) when is_map_key(A, Acc) ->
   false;
 is_marker(_, {Cnt, Size}, _) when Cnt =:= Size ->
