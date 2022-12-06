fe58708379dce56db73f634bca6d448b885146c8
Onno Vos <onno-vos-dev@users.noreply.github.com>
2022-12-06 20:33:55 +0100

    The fastest way I can think of iterating through day 6

--- apps/aoc/src/day06.erl
+++ apps/aoc/src/day06.erl
@@ -9,28 +9,65 @@ solve() ->
 find_markers(Input) ->
   do_find_markers(Input, 4, {0, 0}).
 
-do_find_markers(<<_:8, Rest/binary>> = Bin, Size, {Part1, Cnt}) ->
-  case is_marker(Bin, {0, Size}, #{}) of
+do_find_markers(<<_:8, Rest/binary>> = Bin, Size, {Part1, Pos}) ->
+  case is_marker(Bin, Size) of
     true when Size =:= 4 ->
-      do_find_markers(Rest, 14, {_Part1 = Cnt + Size, Cnt + 1});
+      do_find_markers(Rest, 14, {_Part1 = Pos + Size, Pos + 1});
     true ->
-      {Part1, Cnt + Size};
+      {Part1, _Part2 = Pos + Size};
     false ->
-      do_find_markers(Rest, Size, {Part1, Cnt + 1})
+      do_find_markers(Rest, Size, {Part1, Pos + 1})
   end.
 
-%% Optimized clause that was the solution for part1 can remain
-%% even though it doesn't work for part2.
-%% This lookup is super cheap so this optimized way of checking
-%% whether or not we have a marker can simply remain.
-is_marker(<<A:8, B:8, C:8, D:8, _/binary>>, _, _)
-  when (A =:= B) orelse (A =:= C) orelse (A =:= D) orelse
-       (B =:= C) orelse (B =:= D) orelse
+%% Optimized function clauses for a size of 4 and 14.
+%% Yes this is ugly and a different approach exists in the git history
+%% but this is simply to prove the absolute fastest way we can chew through this in Erlang.
+%% Use of ; is done here since ; compiles differently than orelse and ; is slightly faster.
+-compile({inline, [is_marker/2]}).
+is_marker(<<A:8/integer, B:8/integer, C:8/integer, D:8/integer, _/binary>>, 4 = _Size)
+  when (A =:= B); (A =:= C); (A =:= D);
+       (B =:= C); (B =:= D);
        (C =:= D) ->
   false;
-is_marker(<<A:8, _/binary>>, _, Acc) when is_map_key(A, Acc) ->
+is_marker(<<A:8/integer, B:8/integer, C:8/integer, D:8/integer, E:8/integer, F:8/integer, G:8/integer,
+            H:8/integer, I:8/integer, J:8/integer, K:8/integer, L:8/integer, M:8/integer, N:8/integer, _/binary>>,
+          14 = _Size)
+  when (A =:= B); (A =:= C); (A =:= D); (A =:= E); (A =:= F);
+       (A =:= G); (A =:= H); (A =:= I); (A =:= I); (A =:= J);
+       (A =:= K); (A =:= L); (A =:= M); (A =:= N)
+      ;
+       (B =:= C); (B =:= D); (B =:= E); (B =:= F); (B =:= G);
+       (B =:= H); (B =:= I); (B =:= I); (B =:= J); (B =:= K);
+       (B =:= L); (B =:= M); (B =:= N)
+      ;
+       (C =:= D); (C =:= E); (C =:= F); (C =:= G); (C =:= H);
+       (C =:= I); (C =:= J); (C =:= K); (C =:= L); (C =:= M);
+       (C =:= N)
+      ;
+       (D =:= E); (D =:= F); (D =:= G); (D =:= H); (D =:= I);
+       (D =:= J); (D =:= K); (D =:= L); (D =:= M); (D =:= N)
+      ;
+       (E =:= F); (E =:= G); (E =:= H); (E =:= I); (E =:= J);
+       (E =:= K); (E =:= L); (E =:= M); (E =:= N)
+      ;
+       (F =:= G); (F =:= H); (F =:= I); (F =:= J); (F =:= K);
+       (F =:= L); (F =:= M); (F =:= N)
+      ;
+       (G =:= H); (G =:= I); (G =:= J); (G =:= K); (G =:= L);
+       (G =:= M); (G =:= N)
+      ;
+       (H =:= I); (H =:= J); (H =:= K); (H =:= L); (H =:= M);
+       (H =:= N)
+      ;
+       (I =:= J); (I =:= K); (I =:= L); (I =:= M); (I =:= N)
+      ;
+       (J =:= K); (J =:= L); (J =:= M); (J =:= N)
+      ;
+       (K =:= L); (K =:= M); (K =:= N)
+      ;
+       (L =:= M); (L =:= N)
+      ;
+       (M =:= N) ->
   false;
-is_marker(_, {Cnt, Size}, _) when Cnt =:= Size ->
-  true;
-is_marker(<<A:8, Rest/binary>>, {Cnt, Size}, Acc) ->
-  is_marker(Rest, {Cnt + 1, Size}, Acc#{A => A}).
+is_marker(_, _) ->
+  true.
