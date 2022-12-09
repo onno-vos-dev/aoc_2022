bacfb7c68cea7b3386abceb665a88c4d92c0afde
Onno Vos <onno-vos-dev@users.noreply.github.com>
2022-12-09 13:59:14 +0100

    Slightly better day 8

--- apps/aoc/src/day08.erl
+++ apps/aoc/src/day08.erl
@@ -1,75 +1,98 @@
 -module(day08).
 
 -export([solve/0]).
+-compile([export_all]).
 
 solve() ->
   Input = aoc:read_file("day08.txt", <<"\n">>),
-  MaxX = MaxY = byte_size(hd(Input)),
-  XRange = lists:seq(1, MaxX),
-  YRange = lists:seq(1, MaxY),
-  {ParseT, {GridL, GridM}} = timer:tc(fun() -> grid(XRange, YRange, MaxX, MaxY, Input, {1, []}) end),
-  io:format("ParseT: ~p (~p ms)~n", [ParseT, ParseT div 1000]),
-  {T1, Part1} = timer:tc(fun() -> part1({MaxX, MaxY}, GridM, GridL) end),
-  {T2, Part2} = timer:tc(fun() -> part2(GridM, GridL) end),
-  io:format("T1: ~p (~p ms)~n", [T1, T1 div 1000]),
-  io:format("T2: ~p (~p ms)~n", [T2, T2 div 1000]),
-  {1713,268464} = {Part1, Part2}.
+  {XMap, YMap, Grid} = grid(Input, {1, {#{}, #{}, #{}}}),
+  %% io:format("XMap: ~p~n", [XMap]),
+  %% io:format("YMap: ~p~n", [YMap]),
+  {Tpart1, Part1} = timer:tc(fun() -> part1(XMap, YMap, Grid) end),
+  {Tpart2, Part2} = timer:tc(fun() -> part2(XMap, YMap, Grid) end),
+  io:format("Tpart1: ~p (~p ms)~n", [Tpart1, Tpart1 div 1000]),
+  io:format("Tpart2: ~p (~p ms)~n", [Tpart2, Tpart2 div 1000]),
+  {Part1, Part2}.
 
-grid(_XRange, _YRange, _MaxX, _MaxY, [], {_, Acc}) -> {Acc, maps:from_list(Acc)};
-grid(XRange, YRange, MaxX, MaxY, [H | T], {Y, Acc}) ->
-  {_, NewAcc} = parse_line(XRange, YRange, MaxX, MaxY, H, {{1, Y}, Acc}),
-  grid(XRange, YRange, MaxX, MaxY, T, {Y + 1, NewAcc}).
+grid([], {_, Acc}) -> Acc;
+grid([Line | T], {Y, Acc}) ->
+  NewAcc = parse_line(Line, {{_X = 1, Y}, Acc}),
+  grid(T, {Y + 1, NewAcc}).
 
-parse_line(_XRange, _YRange, _MaxX, _MaxY, <<>>, Acc) -> Acc;
-parse_line(XRange, YRange, MaxX, MaxY, <<H:8, Rest/binary>>, {{X, Y}, Acc}) ->
-  parse_line(XRange, YRange, MaxX, MaxY, Rest, {{X + 1, Y}, [make_coordinate(XRange, YRange, MaxX, MaxY, X, Y, H) | Acc]}).
+parse_line(<<>>, {_, {XMap, YMap, Grid}}) ->
+  {XMap, YMap, Grid};
+parse_line(<<TreeB:8, OtherTrees/binary>>, {{X, Y}, {XMap, YMap, Grid}}) ->
+  Tree = TreeB - $0,
+  NewXMap = maps:update_with(X, fun(V) -> [{X, Tree} | V] end, [{X, Tree}], XMap),
+  NewYMap = maps:update_with(Y, fun(V) -> [{Y, Tree} | V] end, [{Y, Tree}], YMap),
+  NewGrid = Grid#{{X, Y} => Tree},
+  parse_line(OtherTrees, {{X + 1, Y}, {NewXMap, NewYMap, NewGrid}}).
 
-part1({MaxX, MaxY}, GridM, GridL) ->
-  length(lists:filter(fun(Tree) -> is_visible({MaxX, MaxY}, Tree, GridM) end, GridL)).
+part1(XMap, YMap, Grid) ->
+  maps:size(maps:filter(fun(K, V) -> is_visible(XMap, YMap, {K, V}) end, Grid)).
 
-is_visible(_, {_, {Tree, Left, Right, Up, Down}}, GridMap) ->
-  do_is_visible([Left, Right, Up, Down], Tree, GridMap).
-
-do_is_visible([], _Tree, _GridMap) ->
-  false;
-do_is_visible([[] | _], _Tree, _GridMap) ->
+is_visible(_, _, {{X,Y}, _}) when X =:= 1 orelse Y =:= 1 orelse X =:= 99 orelse Y =:= 99 ->
   true;
-do_is_visible([Coordinates | T], Tree, GridMap) ->
-  AllSmaller =
-    lists:all(fun(Coordinate) -> element(1, maps:get(Coordinate, GridMap)) < Tree end, Coordinates),
+is_visible(XMap, YMap, {{X, Y}, Tree}) ->
+  {Up, Down} = split_directions(X, Y, maps:get(X, XMap)),
+  {Left, Right} = split_directions(Y, X, maps:get(Y, YMap)),
+  do_is_visible([Left, Right, Up, Down], Tree).
+
+split_directions(XorY, Skip, List) ->
+  split_directions(XorY, Skip, List, {99, {[], []}}).
+
+split_directions(_, _, [], {_, {L1, L2}}) -> {L1, L2};
+split_directions(XorY, Skip, [{_, Tree} | T], {N, {L1, L2}}) ->
+  case {N > Skip, N < Skip} of
+    {true, false} ->
+      split_directions(XorY, Skip, T, {N - 1, {L1, [Tree | L2]}});
+    {false, true} ->
+      split_directions(XorY, Skip, T, {N - 1, {[Tree | L1], L2}});
+    _ ->
+      split_directions(XorY, Skip, T, {N - 1, {L1, L2}})
+  end.
+
+do_is_visible([], _Tree) ->
+  false;
+do_is_visible([[] | _], _Tree) ->
+  true;
+do_is_visible([Trees | T], Tree) ->
+  AllSmaller = lists:max(Trees) < Tree, %% lists:all(fun(T__) -> T__ < Tree end, Trees),
   case AllSmaller of
     true ->
       true;
     false ->
-      do_is_visible(T, Tree, GridMap)
+      do_is_visible(T, Tree)
   end.
 
-part2(GridM, GridL) ->
-  Scores = lists:map(fun(Tree) -> scenic_score(Tree, GridM) end, GridL),
+part2(XMap, YMap, Grid) ->
+  Scores = maps:fold(fun(K, V, A) -> [scenic_score(XMap, YMap, {K, V})|A] end, [], Grid),
   lists:max(Scores).
 
-scenic_score({_, {Tree, Left, Right, Up, Down}}, GridMap) ->
-  do_scenic_score([lists:reverse(Left), Right, lists:reverse(Up), Down], Tree, GridMap, []).
+scenic_score(XMap, YMap, {{X,Y}, Tree}) ->
+  {Up, Down} = split_directions(X, Y, maps:get(X, XMap)),
+  {Left, Right} = split_directions(Y, X, maps:get(Y, YMap)),
+  do_scenic_score([lists:reverse(Left), Right, lists:reverse(Up), Down], Tree, []).
 
-do_scenic_score([], _Tree, _GridMap, [A, B, C, D]) ->
-  lists:foldl(fun(X, Acc) when X =/= 0 -> Acc * X; (_, Acc) -> Acc end, 1, [A,B,C,D]),
-  A * B * C * D;
-do_scenic_score([Coordinates | T], Tree, GridMap, ScenicScore) ->
-  TallerTrees = foldwhile(Coordinates, fun(Coordinate) -> element(1, maps:get(Coordinate, GridMap)) < Tree end, 0),
-  do_scenic_score(T, Tree, GridMap, [TallerTrees | ScenicScore]).
+do_scenic_score([], _Tree, [A, B, C, D]) ->
+  lists:foldl(fun (X, Acc) when X =/= 0 ->
+                    Acc * X;
+                  (_, Acc) ->
+                    Acc
+              end,
+              1,
+              [A, B, C, D]),
+  ((A * B) * C) * D;
+do_scenic_score([Trees | T], Tree, ScenicScore) ->
+  TallerTrees = foldwhile(Trees, fun(T__) -> T__ < Tree end, 0),
+  do_scenic_score(T, Tree, [TallerTrees | ScenicScore]).
 
-foldwhile([], _, Acc) -> Acc;
+foldwhile([], _, Acc) ->
+  Acc;
 foldwhile([H | T], F, Acc) ->
   case F(H) of
-    true -> foldwhile(T, F, Acc + 1);
-    false -> Acc + 1
+    true ->
+      foldwhile(T, F, Acc + 1);
+    false ->
+      Acc + 1
   end.
-
-make_coordinate(XRange, YRange, MaxX, MaxY, X, Y, H) ->
-  Xs = [X_ || X_ <- XRange, (X =/= 1) orelse (X =/= MaxX)],
-  Ys = [Y_ || Y_ <- YRange, (Y =/= 1) orelse (Y =/= MaxY)],
-  Left = [{X__, Y} || X__ <- Xs, X__ < X],
-  Right = [{X__, Y} || X__ <- Xs, X__ > X],
-  Up = [{X, Y__} || Y__ <- Ys, Y__ < Y],
-  Down = [{X, Y__} || Y__ <- Ys, Y__ > Y],
-  {{X, Y}, {H - $0, Left, Right, Up, Down}}.
